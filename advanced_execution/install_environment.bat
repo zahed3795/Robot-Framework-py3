d757a92f7f88d772be17a3e1134c5e22080a837e
zahed <47490281+zahed3795@users.noreply.github.com>
2020-12-20 01:01:06 -0500

    Update install_environment.bat

--- advanced_execution/install_environment.bat
+++ advanced_execution/install_environment.bat
@@ -2,7 +2,6 @@
 pip install robotframework
 pip install robotframework-pabot
 pip install webdrivermanager
-pip install robotframework-excal
 pip install robotframework-datadriver
 pip install robotframework-requests
 webdrivermanager firefox chrome opera edge --linkpath C:\Users\zahed\AppData\Local\Programs\Python\Python38
