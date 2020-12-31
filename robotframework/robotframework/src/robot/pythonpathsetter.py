556e426949195a14fb26beb4ee9eb52a0681cee5
zahed <47490281+zahed3795@users.noreply.github.com>
2020-12-31 17:42:36 -0500

    Update pythonpathsetter.py

--- robotframework/robotframework/src/robot/pythonpathsetter.py
+++ robotframework/robotframework/src/robot/pythonpathsetter.py
@@ -21,12 +21,14 @@ from os.path import abspath, dirname
 
 ROBOTDIR = dirname(abspath(__file__))
 
+
 def add_path(path, end=False):
     if not end:
         remove_path(path)
         sys.path.insert(0, path)
     elif not any(fnmatch.fnmatch(p, path) for p in sys.path):
         sys.path.append(path)
+
 
 def remove_path(path):
     sys.path = [p for p in sys.path if not fnmatch.fnmatch(p, path)]
