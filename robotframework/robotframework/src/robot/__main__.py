c723fddf39c5a3b5f0979143d9cf6475fd61ef36
zahed <47490281+zahed3795@users.noreply.github.com>
2020-12-31 16:39:17 -0500

    Update __main__.py

--- robotframework/robotframework/src/robot/__main__.py
+++ robotframework/robotframework/src/robot/__main__.py
@@ -19,7 +19,7 @@ import sys
 
 # Allows running as a script. __name__ check needed with multiprocessing:
 # https://github.com/robotframework/robotframework/issues/1137
-if 'robot' not in sys.modules and __name__ == '__main__':
+if 'robot' or 'guild' not in sys.modules and __name__ == '__main__':
     import pythonpathsetter
 
 from robot import run_cli
