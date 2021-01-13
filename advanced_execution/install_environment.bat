53245552d3711996d68ba234957379352e870f91
zahed <47490281+zahed3795@users.noreply.github.com>
2021-01-13 10:43:46 -0500

    Update install_environment.bat

--- advanced_execution/install_environment.bat
+++ advanced_execution/install_environment.bat
@@ -1,29 +1,11 @@
 @ECHO OFF
 pip install webdrivermanager
 webdrivermanager firefox chrome opera edge --linkpath C:\Users\zahed\AppData\Local\Programs\Python\Python38
-cd..
-cd robotframework
-cd robotframework-crypto
-python setup.py install
-cd..
-cd SeleniumLibrary
-python setup.py install
-cd..
-cd robotframework
-python setup.py install
-cd..
-cd rfswarm
-python setup-agent.py install
-python setup-gui.py install
-cd..
-cd robotframework-datadriver
-python setup.py install
-cd..
-cd pabot
-python setup.py install
-cd..
-cd robotframework-requests
-python setup.py install
-cd..
-cd..
+pip install robotframework
+pip install robotframework-crypto
+pip install SeleniumLibrary
+pip install robotframework
+pip install robotframework-datadriver
+pip install pabot
+pip install robotframework-requests
 pip install -U robotframework-datadriver[XLS]
